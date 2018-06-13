a023348e03cae5ad8694b519df265198e31b33fc
Bagrecha <sbagrech@ms.ds.uhc.com>
2018-06-13 15:53:04 +0530

    connection new

--- db_queries/connection.js
+++ db_queries/connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 module.exports = mysql.createPool({
   connectionLimit : 10,
-  host            : 'mysql://mysql:3306/',
+  host            : 'mysql-mymedapp.7e14.starter-us-west-2.openshiftapps.com',
   user            : 'user8TS',
   password        : 'RHlBG40PJ1q1gmEg',
   database        : 'sampledb'
