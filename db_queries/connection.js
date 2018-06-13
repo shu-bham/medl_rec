a28b5f206db4d30d16682d4e73381eba8bd597c3
Bagrecha <sbagrech@ms.ds.uhc.com>
2018-06-13 13:02:32 +0530

    db creds updated

--- db_queries/connection.js
+++ db_queries/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 module.exports = mysql.createPool({
   connectionLimit : 10,
   host            : 'mysql://mysql:3306/',
-  user            : 'user2OK',
-  password        : 't5tcWrfwwiiJrFaQ',
+  user            : 'user8TS',
+  password        : 'RHlBG40PJ1q1gmEg',
   database        : 'sampledb'
 });
