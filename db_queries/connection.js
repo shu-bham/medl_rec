1d1d0c122e25f706b38ddccda2c59d988610312d
Bagrecha <sbagrech@ms.ds.uhc.com>
2018-06-13 15:34:09 +0530

    connection url

--- db_queries/connection.js
+++ db_queries/connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 module.exports = mysql.createPool({
   connectionLimit : 10,
-  host            : '172.30.117.62',
+  host            : 'mysql://mysql:3306/',
   user            : 'user8TS',
   password        : 'RHlBG40PJ1q1gmEg',
   database        : 'sampledb'
